8ddac18a2a812e552a91b175c70ffd47ebccf29f
Nasc1mento <reisadryan16@gmail.com>
2022-11-27 23:27:52 -0300

    '''responsive layout'''

--- src/components/Projects/List.tsx
+++ src/components/Projects/List.tsx
@@ -3,7 +3,7 @@ import React, { Component } from 'react'
 export default class List extends Component {
   render() {
     return (
-      <div className='grid grid-cols-2 p-40'>
+      <div className='md:grid md:grid-cols-2 md:p-40'>
         <div className='justify-center items-start m-5 text-center'>            
             <div className='bg-white rounded py-8'>
                 <h3 className='text-xl text-violet-800 font-bold'>Memory game</h3>
@@ -50,17 +50,6 @@ export default class List extends Component {
                 <div className='flex space-x-2 justify-center m-5'>
                     <button className='bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded duration-500'>
                         <a href="https://github.com/Nasc1mento/ToDoList" target="_blank">Source</a>
-                    </button>
-                </div>   
-            </div>    
-        </div>
-        <div className='justify-center items-center m-5 text-center'>
-            <div className='bg-white rounded py-8'>
-                <h3 className='text-xl text-violet-800 font-bold'>SimpleCalculator</h3>
-                <h4 className='text-lg font-bold text-black'>JavaScript | Svelte</h4>
-                <div className='flex space-x-2 justify-center m-5'>
-                    <button className='bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded duration-500'>
-                        <a href="https://github.com/Nasc1mento/CalculadoraSimples" target="_blank">Source </a>
                     </button>
                 </div>   
             </div>    
