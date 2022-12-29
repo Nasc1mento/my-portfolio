57f78bbf9f3bb2812f154e9c09ace3d7964c855e
Nasc1mento <reisadryan16@gmail.com>
2022-12-28 23:39:32 -0300

    edit projects list

--- src/components/Projects/List.tsx
+++ src/components/Projects/List.tsx
@@ -4,13 +4,13 @@ export default class List extends Component {
   render() {
     return (
       <div className='md:grid md:grid-cols-2 md:p-40'>
-        <div className='justify-center items-start m-5 text-center'>            
+               <div className='justify-center items-start m-5 text-center'>            
             <div className='bg-white rounded py-8'>
-                <h3 className='text-xl text-violet-800 font-bold'>Memory game</h3>
-                <h4 className='text-lg font-bold text-black'>JavaScript | Svelte</h4>
+                <h3 className='text-xl text-violet-800 font-bold'>OOPSocial-RealTimeChat</h3>
+                <h4 className='text-lg font-bold text-black'>Java</h4>
                 <div className='flex space-x-2 justify-center m-5'>
                     <button className='bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded duration-500'>
-                        <a href="https://github.com/Nasc1mento/JogoDaMemoria" target="_blank">Source</a>
+                        <a href="https://github.com/Nasc1mento/OOPSocial-RealTimeChat" target="_blank">Source</a>
                     </button>
                 </div>
             </div>    
@@ -52,6 +52,17 @@ export default class List extends Component {
                         <a href="https://github.com/Nasc1mento/ToDoList" target="_blank">Source</a>
                     </button>
                 </div>   
+            </div>    
+        </div>
+        <div className='justify-center items-start m-5 text-center'>            
+            <div className='bg-white rounded py-8'>
+                <h3 className='text-xl text-violet-800 font-bold'>Memory game</h3>
+                <h4 className='text-lg font-bold text-black'>JavaScript | Svelte</h4>
+                <div className='flex space-x-2 justify-center m-5'>
+                    <button className='bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded duration-500'>
+                        <a href="https://github.com/Nasc1mento/JogoDaMemoria" target="_blank">Source</a>
+                    </button>
+                </div>
             </div>    
         </div>
       </div>
